6ed56b2df57a74a0a9b1e26c4a99286e2a69c05b
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-25 14:38:12 -0800

    Updated css

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,22 @@
 {
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
+	const enter = document.getElementById("loginEnter");
 
+	let correctUsername = "teacher1*";
+	let correctPassword = "schoolname!";
+
+	enter.addEventListener('click', function () {
+		let inputUsername = username.value;
+		let inputPassword = password.value;
+		console.log (inputUsername, inputPassword);
+
+		if (inputUsername != correctUsername | inputPassword != correctPassword) {
+			let errorMessage = document.getElementById("errorMessage");
+			errorMessage.innerHTML = "That is not correct. Please try again.";
+		}
+		else if (inputUsername === correctUsername && inputPassword === correctPassword) {
+			errorMessage.innerHTML = "";
+		}
+	});
 }
