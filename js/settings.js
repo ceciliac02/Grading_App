750cc293d58dbfd1ff209c95bc9a74e63cdf45c9
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-26 12:54:30 -0800

    Updated personal info settings

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,21 @@
 	const readingButton = document.getElementById("reading");
 	const homeworkButton = document.getElementById("homework");
 	const statusButton = document.getElementById("status");
+	const titleButtons = document.querySelectorAll('.nameTitle');
+
+	class LoginInfo {
+		firstName = "Jane";
+		lastName = "Doe";
+		gender = "Female";
+		title = titleButtons[0].checked;
+		email = "doe_jane@school.com";
+	}
+
+	/*for (let i = 0; i < titleButtons.length; i++) {
+		titleButtons[i].addEventListener('click', function () {
+			titleButtons[i]
+		});
+	}*/
 
 	personalSettingsButton.addEventListener('click', function () {
 	});
