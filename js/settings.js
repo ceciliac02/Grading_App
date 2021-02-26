1d5ebb99fa1862946a0a28c732931be306994184
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-25 16:55:14 -0800

    Updated settings page

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+{
+	const personalSettingsButton = document.getElementById("personalSettings");
+	const readingButton = document.getElementById("reading");
+	const homeworkButton = document.getElementById("homework");
+	const statusButton = document.getElementById("status");
+
+	personalSettingsButton.addEventListener('click', function () {
+	});
+}
