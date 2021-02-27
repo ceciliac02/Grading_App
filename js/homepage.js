4ae10db00a3c8628de1c96003cd82d00c83a3d58
Cecilia Cruz <cruzscecilia@gmail.com>
2021-02-27 11:45:59 -0800

    Updated login form code

--- js/homepage.js
+++ js/homepage.js
@@ -7,27 +7,45 @@
 	let correctPassword = "schoolname!";
 	let login = false;
 
-	enter.addEventListener('click', function () {
-		let inputUsername = username.value;
-		let inputPassword = password.value;
-		console.log (inputUsername, inputPassword);
+	enter.addEventListener('click', function () { 
+		displayErrorMessage(verifyLogin(username.value, password.value)),
+		loggedInStatus(verifyLogin(username.value, password.value))
+	});
 
-		if (inputUsername != correctUsername | inputPassword != correctPassword) {
-			let errorMessage = document.getElementById("errorMessage");
+	function verifyLogin (usernameInput, passwordInput) {
+		this.usernameInput = usernameInput;
+		this.passwordInput = passwordInput;
+
+		let verification;
+		
+		if (usernameInput === correctUsername && passwordInput === correctPassword) {
+			verification = true;
+		}
+		else if (usernameInput != correctUsername || passwordInput != correctPassword) {
+			verification = false;
+		}
+		console.log(verification);
+		return verification;
+	}
+
+	function displayErrorMessage (verification) {
+		let errorMessage = document.getElementById("errorMessage");
+		if (verification === false) {
 			errorMessage.innerHTML = "That is not correct. Please try again.";
 		}
-		else if (inputUsername === correctUsername && inputPassword === correctPassword) {
+		else if (verification === true) {
 			errorMessage.innerHTML = "";
-			login = true;
-			console.log(login);
 		}
-		if (login === true) {
-			let loginForm = document.getElementById("loginForm");
-			let logout = document.getElementById("logout");
-			loginForm.innerHTML = "Logged in";
-			loginForm.style.textAlign = "center";
-			logout.style.display = "initial";
-			console.log("Logged in");
+	}
+
+	function loggedInStatus (verification) {
+		if(verification === true) {
+		let loginForm = document.getElementById("loginForm");
+		let logout = document.getElementById("logout");
+		loginForm.innerHTML = "Logged in";
+		loginForm.style.textAlign = "center";
+		logout.style.display = "initial";
+		console.log("Logged in");
 		}
-	});
+	}
 }
